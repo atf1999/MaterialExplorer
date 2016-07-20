bdec5265bb05d2d28c42f14c8313259508acb36e
atf1999 <torth212@gmail.com>
2016-07-20 17:08:33 -0400

    fixed ugly space issue. curse the atom text editor

--- main.js
+++ main.js
@@ -11,20 +11,20 @@ const ipcMain = electron.ipcMain;
 let win;
 
 function createWindow() {
-	  // Create the browser window.
-	  win = new BrowserWindow({width: 800, height: 600});
+  // Create the browser window
+  win = new BrowserWindow({width: 800, height: 600});
 
-    // and load the index.html of the app.
-	  win.loadURL(`file://${ __dirname}/index.html`);
-    win.webContents.openDevTools();
+  // and load the index.html of the app.
+  win.loadURL(`file://${ __dirname}/index.html`);
+  win.webContents.openDevTools();
 
-	  // Emitted when the window is closed.
-	  win.on('closed', () => {
-		  // Dereference the window object, usually you would store windows
-		  // in an array if your app supports multi windows, this is the time
-		  // when you should delete the corresponding element.
-	      win = null;
-	  });
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
 }
 
 // This method will be called when Electron has finished
@@ -34,17 +34,17 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-	  // On macOS it is common for applications and their menu bar
-	    // to stay active until the user quits explicitly with Cmd + Q
-		  if (process.platform !== 'darwin') {
-			      app.quit();
-	    }
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
-	  // On macOS it's common to re-create a window in the app when the
-	    // dock icon is clicked and there are no other windows open.
-		  if (win === null) {
-			      createWindow();
-	    }
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow();
+  }
 });
